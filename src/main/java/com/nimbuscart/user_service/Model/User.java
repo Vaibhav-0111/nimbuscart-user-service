a9da13d11bda9149abfb06e35cbd66fed9531ec8
Vaibhav-0111 <vaibhavtripathi724@gmail.com>
2026-07-23 23:18:12 +0530

    Day 3: Added Bean Validation (@NotBlank, @Email) and centralized error handling with @RestControllerAdvice

--- src/main/java/com/nimbuscart/user_service/Model/User.java
+++ src/main/java/com/nimbuscart/user_service/Model/User.java
@@ -1,23 +1,33 @@
 package com.nimbuscart.user_service.Model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "users")
-public class User{
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Name is required")
     @Column(nullable = false)
     private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
     @Column(nullable = false, unique = true)
     private String email;
+
     public User() {}
+
     public User(String name, String email) {
         this.name = name;
         this.email = email;
     }
+
     public Long getId() {
         return id;
     }
@@ -31,9 +41,11 @@ public class User{
     public void setName(String name) {
         this.name = name;
     }
+
     public String getEmail() {
         return email;
     }
-    public void setEmail(String email) { this.email = email;
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
